f15eabe2031ee4a2cb0e352912c47591db30b2db
Søren Mark <smark484@gmail.com>
2015-01-31 19:27:37 +0100

    compability
    changed LocationLogAdapter to extend support v4 CursorAdapter

--- app/src/main/java/android/example/com/locationlogger/LocationLogAdapter.java
+++ app/src/main/java/android/example/com/locationlogger/LocationLogAdapter.java
@@ -3,10 +3,10 @@ package android.example.com.locationlogger;
 import android.content.Context;
 import android.database.Cursor;
 import android.location.Location;
+import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 import android.example.com.locationlogger.LocationContract.LocationEntry;
